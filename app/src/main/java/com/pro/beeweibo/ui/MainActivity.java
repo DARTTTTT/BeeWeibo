831e89fc21c7eb7b23e4b724162e6be91340be3f
aaron <aaronstepbystep@gmail.com>
2021-04-22 11:19:22 +0800

    2

--- app/src/main/java/com/pro/beeweibo/ui/MainActivity.java
+++ app/src/main/java/com/pro/beeweibo/ui/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import com.pro.beeeibo.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
