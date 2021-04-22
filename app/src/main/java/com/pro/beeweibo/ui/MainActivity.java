2be253d7d2d60e7f8c78ee61faefbc6e5a4761d7
aaron <aaronstepbystep@gmail.com>
2021-04-22 11:17:23 +0800

    Initial commit

---
+++ app/src/main/java/com/pro/beeweibo/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pro.beeweibo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pro.beeeibo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
